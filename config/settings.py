222cbda3e7c91950f56930570f977c71d99f6ffb
blingblin-g <luvbliny@gmail.com>
2021-10-27 01:07:27 +0900

    prepare to deploy

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,20 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+...
+import django_heroku
+...
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+...
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    ...
+]
+...
+# Activate Django-Heroku.
+django_heroku.settings(locals())
